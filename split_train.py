b37da51bd63d198f3e670cc264b659820d5c3918
matthiasdellago <matthias.dellago@proton.me>
2024-01-14 17:34:04 +0100

    changed params for shakespeare finetune

--- split_train.py
+++ split_train.py
@@ -49,21 +49,21 @@ qk_weight_decay = 0.1
 # -----------------------------------------------------------------------------
 # default config values designed to train a gpt2 (124M) on OpenWebText
 # I/O
-out_dir = 'out-shakespeare-char'
-eval_interval = 250
-log_interval = 10
-eval_iters = 200
+out_dir = 'out-shakespeare'
+eval_interval = 5
+log_interval = 5
+eval_iters = 40
 eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = False # if True, always save a checkpoint after each eval
-init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
+init_from = 'gpt2' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
 wandb_log = True # disabled by default 
-wandb_project = 'shakespeare-char'
-wandb_run_name = 'test' + str(time.time())
+wandb_project = 'shakespeare-finetune'
+wandb_run_name = 'cluster' + str(time.time())
 # data
-dataset = 'shakespeare_char'
-gradient_accumulation_steps = 1 # used to simulate larger batch sizes
-batch_size = 12 # if gradient_accumulation_steps > 1, this is the micro-batch size
+dataset = 'shakespeare'
+gradient_accumulation_steps = 32 # used to simulate larger batch sizes
+batch_size = 1 # if gradient_accumulation_steps > 1, this is the micro-batch size
 block_size = 64
 # model
 n_layer = 4
@@ -72,14 +72,14 @@ n_embd = 128
 dropout = 0.0 # for pretraining 0 is good, for finetuning try 0.1+
 bias = False # do we use bias inside LayerNorm and Linear layers?
 # adamw optimizer
-learning_rate = 1e-3 # max learning rate
-max_iters = 2000 # total number of training iterations
-weight_decay = 1e-1
+learning_rate = 3e-5 # max learning rate
+max_iters = 20 # total number of training iterations
+weight_decay = 0.1
 beta1 = 0.9
 beta2 = 0.99
 grad_clip = 1.0 # clip gradients at this value, or disable if == 0.0
 # learning rate decay settings
-decay_lr = True # whether to decay the learning rate
+decay_lr = False # whether to decay the learning rate
 warmup_iters = 20 # how many steps to warm up for
 lr_decay_iters = max_iters # should be ~= max_iters per Chinchilla
 min_lr = learning_rate/10 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
